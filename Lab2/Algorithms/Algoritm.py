0363699adaf377ea0d5afa342994b247a95801f4
Dadmon <maksklym-ipt27@lll.kpi.ua>
2025-03-12 00:46:44 +0200

    Done Algorithms

--- Lab2/Algorithms/Algoritm.py
+++ Lab2/Algorithms/Algoritm.py
@@ -5,11 +5,13 @@ import matplotlib.animation as animation
 
 
 class Algorithm:
-    def __init__(self, pop_size, iterations, function, limits, **kwargs):
+    def __init__(self, pop_size, iterations, random_limits, function, limits, **kwargs):
         self.iterations = iterations
         self.pop_size = pop_size
         self.function = function
         self.limits = np.array(limits)
+
+        [self.low, self.high] = random_limits
 
         self.dim = len(self.limits)
 
@@ -17,8 +19,10 @@ class Algorithm:
 
         self.kwargs = kwargs
 
-        self.x_low = [limit[0] for limit in self.limits]
-        self.x_high = [limit[1] for limit in self.limits]
+        # self.x_low = [limit[0] for limit in self.limits]
+        # self.x_high = [limit[1] for limit in self.limits]
+        self.x_low = self.limits[:, 0]
+        self.x_high = self.limits[:, 1]
         self.parts = np.random.uniform(self.x_low, self.x_high, (self.pop_size, self.dim))
         for i in self.integer:
             self.parts[:, i] = np.round(self.parts[:, i])
@@ -33,6 +37,34 @@ class Algorithm:
         self.history_best_dep_val = []
         self.history_best = []
 
+        self.same = False
+
+        self.cnt_max = kwargs.get("count", 3)
+        self.cnt = 0
+        self.epsilon = kwargs.get("epsilon", 0.0000001)
+        self.progress = kwargs.get("progress", True)
+
+    def check_if_same(self, prev_best, new_best):
+        if abs(new_best - prev_best) < self.epsilon:
+            self.cnt += 1
+            if self.cnt == self.cnt_max:
+                self.same = True
+                return True
+            return False
+        self.cnt = 0
+        return False
+
+    def progress_bar(self, quantity, total, **kwargs):
+        if not self.progress:
+            return
+        name = kwargs.get("name", "No Name")
+        percent = (quantity / total) * 100
+        print(f"Progress {name} : {quantity}/{total} ({percent:.2f}%)", end='\r')
+        if quantity == total-1:
+            percent = ((quantity+1) / total) * 100
+            print(f"Progress {name} : {quantity+1}/{total} ({percent:.2f}%)")
+            # print()
+
     def plot(self, **kwargs):
         dots = kwargs.get("dots", 500)
 
@@ -42,24 +74,27 @@ class Algorithm:
         save_path = kwargs.get("save", None)
 
         show = kwargs.get("show", False)
+        plot = kwargs.get("plot", True)
 
         if show:
-            plt.plot(list(range(self.iterations)), self.history_best)
+            plt.plot(list(range(self.iterations))[:len(self.history_best)], self.history_best, label=self.__class__.__name__)
             plt.grid()
-            plt.show()
+            plt.legend()
+            if plot:
+                plt.show()
 
         if self.dim <= 2 and (show or save_path is not None):
-            self.projection_dep_val = np.linspace(self.x_low, self.x_high, dots)
-            space = np.array([self.projection_dep_val[:, i] for i in range(self.dim)])
-            space = np.meshgrid(*space)
-            self.projection = np.array([[self.function([space[i][j, k] for i in range(self.dim)]) for k in range(dots)] for j in range(dots)])
-
             if d2:
                 fig, ax1 = plt.subplots()
             elif d3:
                 fig = plt.figure(figsize=plt.figaspect(2.))
                 ax1 = fig.add_subplot(1, 1, 1, projection='3d')
             if (d2 or d3):
+                self.projection_dep_val = np.linspace(self.x_low, self.x_high, dots)
+                space = np.array([self.projection_dep_val[:, i] for i in range(self.dim)])
+                space = np.meshgrid(*space)
+                self.projection = np.array([[self.function([space[i][j, k] for i in range(self.dim)]) for k in range(dots)] for j in range(dots)])
+
                 cs = ax1.contourf(*space, self.projection, cmap="cool")
                 fig.colorbar(cs)
 
@@ -77,7 +112,7 @@ class Algorithm:
                         ax1.scatter(*[self.history_best_dep_val[frame][i] for i in range(self.dim)], self.history_best[frame], label="Best", c='Red')
                     ax1.legend()
 
-                ani = animation.FuncAnimation(fig=fig, func=update, frames=self.iterations, interval=10)
+                ani = animation.FuncAnimation(fig=fig, func=update, frames=min(self.iterations, len(self.history_best)), interval=10)
                 if save_path is not None:
                     ani.save(save_path, fps=10)
                 if show:
--- Lab2/Algorithms/Firefly.py
+++ Lab2/Algorithms/Firefly.py
@@ -2,16 +2,18 @@ import numba as nb
 
 import numpy as np
 
-from Algoritm import Algorithm
+if __name__ == "__main__":
+    from Algoritm import Algorithm
+else:
+    from .Algoritm import Algorithm
 
 
 class FF(Algorithm):
-    def __init__(self, pop_size, iterations, low_high, function, limits, **kwargs): # , beta_max, gamma, alpha
-        super().__init__(pop_size, iterations, function, limits, **kwargs)
+    def __init__(self, pop_size, iterations, random_limits, function, limits, **kwargs): # , beta_max, gamma, alpha
+        super().__init__(pop_size, iterations, random_limits, function, limits, **kwargs)
         # self.beta_max = beta_max
         # self.gamma = gamma
         # self.alpha = alpha
-        [self.low, self.high] = low_high
 
     def run(self, **kwargs):
         # @nb.jit
@@ -24,34 +26,45 @@ class FF(Algorithm):
         #                 parts[k] = parts[k] + beta * (parts[l] - parts[k]) + alpha * (np.random.rand(dim)-0.5)
         #                 parts[k] = np.maximum(limits[:, 0], np.minimum(limits[:, 1], parts[k]))
         #     return parts
+        # self.parts = run_through(self.pop_size, self.parts, self.function, self.beta_max, self.gamma, self.alpha, self.limits, self.dim)
         # firefly_indexes = np.where(self.fitness_func < self.function(self.parts[k]))[0]
         self.kwargs.update(kwargs)
         show = self.kwargs.get("show", False)
         save_location = self.kwargs.get("save", None)
-        self.beta_max = np.random.uniform(self.low, self.high)
-        self.gamma = np.random.uniform(self.low, self.high)
-        self.alpha = np.random.uniform(self.low, self.high)
-        for n in range(self.iterations):
-            # if n % np.random.randint(2, 5) == 0:
-            if n % 2 == 0:
-                self.beta_max = np.random.uniform(self.low, self.high)
-                self.gamma = np.random.uniform(self.low, self.high)
-                self.alpha = np.random.uniform(self.low, self.high)
-            # self.parts = run_through(self.pop_size, self.parts, self.function, self.beta_max, self.gamma, self.alpha, self.limits, self.dim)
+        # self.beta_max = np.random.uniform(self.low, self.high)
+        # self.gamma = np.random.uniform(self.low, self.high)
+        # self.alpha = np.random.uniform(self.low, self.high)
+        for iteration in range(self.iterations):
+            self.progress_bar(iteration, self.iterations, name="Firefly")
+            self.parts = self.parts[np.argsort(self.fitness_func)]
+            self.fitness_func = np.sort(self.fitness_func)
+            # if iteration % 2 == 0:
+            self.beta_max = np.random.uniform(self.low, self.high)
+            self.gamma = np.random.uniform(self.low, self.high)
+            self.alpha = np.random.uniform(self.low, self.high)
             for k in range(self.pop_size):
                 for l in range(self.pop_size):
-                    if self.function(self.parts[l]) < self.function(self.parts[k]):
-                        d = np.sqrt(np.sum([(y-x)**2 for x, y in zip(self.parts[k], self.parts[l])]))
+                    # self.beta_max = np.random.uniform(self.low, self.high)  
+                    # self.gamma = np.random.uniform(self.low, self.high)
+                    # self.alpha = np.random.uniform(self.low, self.high)
+                    # if self.function(self.parts[l]) < self.function(self.parts[k]):
+                    if self.fitness_func[l] < self.fitness_func[k]:
+                        d = np.linalg.norm(self.parts[k] - self.parts[l])
+                        # d = np.sqrt(np.sum([(y-x)**2 for x, y in zip(self.parts[k], self.parts[l])]))
                         beta = self.beta_max*np.exp(-self.gamma*d**2)
                         self.parts[k] = self.parts[k] + beta * (self.parts[l] - self.parts[k]) + self.alpha * (np.random.rand(self.dim)-0.5)
-                        self.parts[k] = np.maximum(self.limits[:, 0], np.minimum(self.limits[:, 1], self.parts[k]))
-            for i in self.integer:
-                self.parts[:, i] = np.round(self.parts[:, i])
-            self.fitness_func = np.array([self.function(part) for part in self.parts])
+                        # self.parts[k] = np.clip(self.parts[k], self.limits[:, 0], self.limits[:, 1])
+                        self.parts[k] = np.clip(self.parts[k], self.x_low, self.x_high)
+                        for i in self.integer:
+                            self.parts[k, i] = np.round(self.parts[k, i])
+                        self.fitness_func[k] = self.function(self.parts[k])
             ks = np.argmin(self.fitness_func)
             if self.function(self.parts[ks]) < self.best:
                 self.best_dep_val = self.parts[ks]
                 self.best = self.function(self.best_dep_val)
+
+            if self.best != float("inf") and len(self.history_best) != 0:
+                self.check_if_same(self.best, self.history_best[-1])
 
             if show or save_location is not None:
                 self.history_parts.append(self.parts.copy())
@@ -59,6 +72,16 @@ class FF(Algorithm):
 
                 self.history_best.append(self.best)
                 self.history_best_dep_val.append(self.best_dep_val.copy())
+            if iteration != 0 and iteration % 10 == 0:
+                # if (np.array(self.history_best[iteration-10:iteration]) == float("inf")).all():
+                if np.unique(np.array(self.history_best[iteration-10:iteration])).size == 1:
+                    self.parts = np.random.uniform(self.x_low, self.x_high, (self.pop_size, self.dim))
+                    for i in self.integer:
+                        self.parts[:, i] = np.round(self.parts[:, i])
+                    self.fitness_func = np.array([self.function(part) for part in self.parts])
+
+            if self.same:
+                break
         if show or save_location is not None:
             self.plot(**self.kwargs)
         return self.best, self.best_dep_val
@@ -67,14 +90,14 @@ class FF(Algorithm):
 if __name__ == "__main__":
     # -------------------Rastrigin-------------------- #
     # @nb.jit
-    def F(X):
-        A = 10
-        length = len(X)
-        result = A*length
-        for x in X:
-            result += x**2-A*np.cos(2*np.pi*x)
-        return result
-    ff = FF(50, 50, [0, 1], F, [[-5.12, 5.12], [-5.12, 5.12]], d2=True, show=True).run()
+    # def F(X):
+    #     A = 10
+    #     length = len(X)
+    #     result = A*length
+    #     for x in X:
+    #         result += x**2-A*np.cos(2*np.pi*x)
+    #     return result
+    # ff = FF(50, 50, [0, 1], F, [[-5.12, 5.12], [-5.12, 5.12]], d2=True, show=True).run()
 
     # ------------------Rozenbrock------------------- #
     # def F(X):
@@ -100,7 +123,7 @@ if __name__ == "__main__":
     #     else:
     #         return float('inf')
 
-    # ff = FF(50, 50, [0, 1], F, [[-1.5, 1.5], [-1.5, 1.5]], d2=True, show=True).run() # , save="Lab2/FF.gif"
+    # ff = FF(50, 50, [0, .5], F, [[-1.5, 1.5], [-1.5, 1.5]], d2=True, show=True).run() # , save="Lab2/FF.gif"
 
     # -------------------Mishri-Berda------------------------------ #
     # def F(X):
@@ -125,39 +148,28 @@ if __name__ == "__main__":
     # ff = FF(50, 50, [0, 1], F, [[-1.25, 1.25], [-1.25, 1.25]], d2=True, show=True).run()
 
     # -----------------Reductor---------------------- #
-    # @nb.jit
-    # def F(X):
-    #     x1, x2, x3, x4, x5, x6, x7, = X
-    #     f1 = 27/(x1*x2**2*x3) - 1  < 0
-    #     if f1:
-    #         f2 = 397.5/(x1*x2**2*x3**2) - 1 < 0
-    #         if f2:
-    #             f3 = 1.93*x4**3/(x2*x3*x6**2) - 1 < 0
-    #             if f3:
-    #                 f4 = 1.93/(x2*x3*x7**4) - 1 < 0
-    #                 if f4:
-    #                     f5 = 1.0/(110*x6**3) * np.sqrt(((745*x4)/(x2*x3))**2 + 16.9 * 10**6) - 1 < 0
-    #                     if f5:
-    #                         f6 = 1.0/(85*x7**3) * np.sqrt(((745*x5)/(x2*x3))**2 + 157.5 * 10**6) - 1 < 0
-    #                         if f6:
-    #                             f7 = x2*x3/40 - 1 <= 0
-    #                             if f7:
-    #                                 f8 = 5*x2/x1 - 1 <= 0
-    #                                 if f8:
-    #                                     f9 = x1/(12*x2) - 1 <= 0
-    #                                     if f9:
-    #                                         f10 = (1.5*x6 + 1.9)/x4 - 1 <= 0
-    #                                         if f10:
-    #                                             f11 = (1.1*x7 + 1.9)/x5 - 1 <= 0
-    #                                             if f11:
-    #                                                 return 0.7854*x1*x2**2*(3.3333*x3**2 + 14.9334*x3 - 43.0934) - 1.508*x1*(x6**2 + x7**2) + 7.4777*(x6**3 + x7**3) + 0.7854*(x4*x6**2 + x5*x7**2)
-    #     return float('inf')
+    @nb.jit
+    def F(X):
+        x1, x2, x3, x4, x5, x6, x7, = X
+        f1 = 27 / (x1 * x2**2 * x3) - 1  <= 0
+        f2 = 397.5 / (x1 * x2**2 * x3**2) - 1 <= 0
+        f3 = 1.93 * x4**3 / (x2 * x3 * x6**4) - 1 <= 0
+        f4 = 1.93 / (x2 * x3 * x7**4) - 1 <= 0
+        f5 = 1.0/(110 * x6**3) * np.sqrt(((745*x4) / (x2 * x3))**2 + 16.9 * 10**6) - 1 <= 0
+        f6 = 1.0/(85 * x7**3) * np.sqrt(((745*x5) / (x2 * x3))**2 + 157.5 * 10**6) - 1 <= 0
+        f7 = (x2*x3) / 40 - 1 <= 0
+        f8 = 5*x2 / x1 - 1 <= 0
+        f9 = x1 / (12 * x2) - 1 <= 0
+        f10 = (1.5 * x6 + 1.9) / x4 - 1 <= 0
+        f11 = (1.1 * x7 + 1.9) / x5 - 1 <= 0
+        if f1 and f2 and f3 and f4 and f5 and f6 and f7 and f8 and f9 and f10 and f11:
+            return 0.7854*x1*x2**2*(3.3333*x3**2 + 14.9334*x3 - 43.0934) - 1.508*x1*(x6**2 + x7**2) + 7.4777*(x6**3 + x7**3) + 0.7854*(x4*x6**2 + x5*x7**2)
+        return float('inf')
+        
 
-    # for i in range(100):
-    # ff = FF(400, 50, [0, 2], F, [[2.6, 3.6], [0.7, 0.8], [17, 28], [7.3, 8.3], [7.8, 8.3], [2.9, 3.9], [5.0, 5.5]], d2=True, show=False, integer=[2])
-    # result = ff.run()
-    # print(*result)
-    # print(F(result[1]))
+    ff = FF(400, 500, [0.1, 1], F, [[2.6, 3.6], [0.7, 0.8], [17, 28], [7.3, 8.3], [7.8, 8.3], [2.9, 3.9], [5.0, 5.5]], d2=False, show=True, integer=[2])
+    result = ff.run()
+    print(*result)
 
     # -----------------Trail----------------------------- #
     # def F(X):
